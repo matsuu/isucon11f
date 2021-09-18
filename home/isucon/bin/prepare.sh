35ded4d378df62133dd84aa060d7af96e7c56d3b
ウー馬場ーイー2 <ubabar-eat2@isucon.net>
2021-09-18 04:47:08 +0000

    68795

--- home/isucon/bin/prepare.sh
+++ home/isucon/bin/prepare.sh
@@ -6,14 +6,17 @@ set -ex
   make
 )
 
-# sudo rsync -av --delete isu2:/home/isucon/webapp/ /home/isucon/webapp/
-# sudo rsync -av --delete isu2:/home/isucon/env.sh /home/isucon/env.sh
-# sudo rsync -av --delete isu2:/etc/mysql/ /etc/mysql/
+for s in isu2 isu3 ; do
+  sudo rsync -av --delete /home/isucon/bin/ $s:/home/isucon/bin/
+  sudo rsync -av --delete /home/isucon/webapp/ $s:/home/isucon/webapp/
+  sudo rsync -av --delete /home/isucon/env.sh $s:/home/isucon/env.sh
+  sudo rsync -av --delete /etc/mysql/ $s:/etc/mysql/
+done
 
 # mysql
-sudo mysql -e "TRUNCATE TABLE performance_schema.events_statements_summary_by_digest"
-sudo rm -f /var/log/mysql/mysql-slow.log
-sudo mysqladmin flush-logs
+#sudo ssh isu3 mysql -e "TRUNCATE TABLE performance_schema.events_statements_summary_by_digest"
+sudo ssh isu3 rm -f /var/log/mysql/mysql-slow.log
+sudo ssh isu3 mysqladmin flush-logs
 
 # app
 sudo systemctl restart isucholar.go
