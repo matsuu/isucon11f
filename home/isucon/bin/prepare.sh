d30888270eca5fadeabf24563750bfff46bfb134
ウー馬場ーイー2 <ubabar-eat2@isucon.net>
2021-09-18 01:22:08 +0000

    31928

---
+++ home/isucon/bin/prepare.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+set -ex
+
+(
+  cd /home/isucon/webapp/go
+  go build
+)
+
+# sudo rsync -av --delete isu2:/home/isucon/webapp/ /home/isucon/webapp/
+# sudo rsync -av --delete isu2:/home/isucon/env.sh /home/isucon/env.sh
+# sudo rsync -av --delete isu2:/etc/mysql/ /etc/mysql/
+
+# mysql
+sudo mysql -e "TRUNCATE TABLE performance_schema.events_statements_summary_by_digest"
+#sudo rm -f /var/log/mysql/mysql-slow.log
+#sudo mysqladmin flush-slow-log
+
+# app
+sudo systemctl restart isucholar.go
+
+# nginx
+sudo rm -f /var/log/nginx/access.log
+sudo systemctl reload nginx
+
+echo "OK"
